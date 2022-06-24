c14c0bf3beabd3febf093a9d9af39693f801bfe3
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-24 21:54:07 +0100

    finding combination

--- js/Game.js
+++ js/Game.js
@@ -27,6 +27,24 @@ export default class Game {
             return;
         }
          this.board[i] = this.turn; // X or O
+         this.findWinningCombinations();
+    }
+
+    findWinningCombinations(){
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2]
+        ]
+        
+        for(const combination of winningCombinations){
+            console.log(combination);
+        }
     }
 
 
